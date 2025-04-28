e761e51b498f90d48be76d6e76f38b6819de1877
Daniel Hyman <danielHyman20@gmail.com>
2025-04-28 18:20:48 -0400

    Create Note button
    This button opens note.html to create a note

---
+++ script/index.js
@@ -0,0 +1,11 @@
+// function openWindow(){
+//     window.open(
+//         'notes.html', 
+//         '_blank', 
+//         'width=800,height=600,scrollbars=yes,resizable=yes'
+//     );
+// }
+
+function openMenu() {
+    window.open("notes.html", "NoteTaker", "width=600,height=400");
+  }
