838080a1e3c63ee67e2cb332f8bc5c923173e3bc
Daniel Hyman <danielHyman20@gmail.com>
2025-04-28 20:00:01 -0400

    Create Note Button
    Added Create Note Button which opens a new window to create a note.

--- script/index.js
+++ script/index.js
@@ -1,11 +1,9 @@
-// function openWindow(){
-//     window.open(
-//         'notes.html', 
-//         '_blank', 
-//         'width=800,height=600,scrollbars=yes,resizable=yes'
-//     );
-// }
-
-function openMenu() {
+createNoteButton = document.getElementsByTagName("button");
+but = document.getElementById("createNoteButton")
+console.log(createNoteButton);
+console.log(but);
+console.log("Button clicked");
+but.addEventListener('click', () => { //() => is an on the spot function creator.
     window.open("notes.html", "NoteTaker", "width=600,height=400");
-  }
+    console.log("Button clicked again");
+ });
