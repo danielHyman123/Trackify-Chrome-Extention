afaea22f6d42a5440cad87391ae5d962fd0a6e75
peterfrach <peterfrach1@gmail.com>
2025-04-30 18:20:41 -0400

    Update popup.js

--- script/popup.js
+++ script/popup.js
@@ -1,4 +1,19 @@
-document.getElementById('toggleSidebar').addEventListener('click', async () => {
+// document.getElementById('toggleSidebar').addEventListener('click', async () => {
+//     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+//     chrome.scripting.executeScript({
+//       target: { tabId: tab.id },
+//       files: ['script/injectSidebar.js']
+//     });
+// });
+
+
+toggleSidebar = document.getElementsByTagName("toggleSidebar");
+bar = document.getElementById("toggleSidebar")
+console.log(toggleSidebar);
+console.log(bar);
+console.log("Button clicked");
+bar.addEventListener('click', async () => { //() => is an on the spot function creator.
     const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   
     chrome.scripting.executeScript({
